c39397b097e3f8dc91dfbdadabbf2e70b3b29053
jduan7 <129612251+jduan7@users.noreply.github.com>
2026-02-23 14:53:09 -0500

    Update Fig4_nuclease.R

--- Fig4_nuclease.R
+++ Fig4_nuclease.R
@@ -1,4 +1,6 @@
-#Figure 4. Assessing the stability of viral genomes within viral particles in the presence of nucleases.
+# This script performs Spearmam's Correlations on the nuclease treatment experiment
+# This test detects any monotonic trend (even if nonlinear) and reports one p-value per virus
+
 library(data.table)
 library(readxl)
 library(ggpubr)
@@ -7,11 +9,11 @@ library(dplyr)
 library(patchwork)
 library(plotly)
 library(paletteer)
-
+library(ggrepel)
 
 ############Sequencing plot
 # Get sequencing data
-setwd("/Users/jduan/bushman/virome_methods/paper/codes/codes for publication/")
+setwd("/Users/jduan/bushman/virome_methods/250429_nextseq/R analysis")
 ref_viral_analysis<-read_excel("250505_nextseq_R_analysis.xlsx", sheet="ref_viral_analysis")
 ref_viral_percentage<-read_excel("250505_nextseq_R_analysis.xlsx", sheet="ref_viral_percentage")
 
@@ -24,108 +26,134 @@ nuc_ref_viral_abundance <- ref_viral_abundance %>% filter(Experiment=="nuclease 
 nuc_ref_viral_abundance$Nuclease <- factor(nuc_ref_viral_abundance$Nuclease,
                                            levels = c("none", "0.1X", "1X", "10X"))  # or your actual order
 
-summary_nuc_ref_viral_abundance <- nuc_ref_viral_abundance %>%
-  filter(is.finite(ref_relative_abundance)) %>%
-  group_by(Nuclease, reference_genome) %>%
-  summarise(mean_abundance = mean(ref_relative_abundance), .groups = "drop")
+# Compute log2 fold change relative to untreated
+nuc_norm_abundance <- nuc_ref_viral_abundance %>% filter(Mock_Community=="mock community spiked-in") %>%
+  group_by(reference_genome) %>%
+  mutate(untreated_mean = mean(ref_relative_abundance[Nuclease == "none"])) %>% #calculate the mean of untreated
+  ungroup() %>%
+  mutate(log2FC = log2((ref_relative_abundance+1e-6)/untreated_mean)) #add pseudo value to avoid zero, and normalize other values to the mean of untreated
 
-# Calculate and plot for relative abundance normalized to the no treatment
-norm_abundance <- summary_nuc_ref_viral_abundance %>% filter(Nuclease=="none") %>% rename(no_nuclease_abundance=mean_abundance) #get the no_nuclease relative abundance
-nuc_norm_abundance <- nuc_ref_viral_abundance %>% left_join(norm_abundance %>% ungroup() %>% select (reference_genome,no_nuclease_abundance),
-                                                            by = c("reference_genome" = "reference_genome")) #join the table by matching the no_nuclease relative abundance value for each nuclease group according to reference genome
-nuc_norm_abundance <- nuc_norm_abundance %>% mutate(normalized_abundance = ref_relative_abundance/no_nuclease_abundance) #normalize the relative abundance of each nuclease treatment group by the no_treatment relative abundance value
+# Encode nuclease numerically for regression
+nuc_norm_abundance <- nuc_norm_abundance %>%
+  mutate(Dose = case_when(
+    Nuclease == "none" ~ 0,
+    Nuclease == "0.1X"     ~ 0.1,
+    Nuclease == "1X"       ~ 1,
+    Nuclease == "10X"      ~ 10
+  ),
+  logDose = log10(Dose + 0.01))  # small offset to avoid log10(0)
 
-summary_norm_abundance <- nuc_norm_abundance %>%
-  filter(is.finite(ref_relative_abundance)) %>%
-  group_by(Nuclease, reference_genome) %>%
-  summarise(mean_norm_abundance = mean(normalized_abundance), .groups = "drop") #make a summary table of the mean of normalized abundance for triplicates
 
-# Make plot
-nuc_VC_seq_plot <- ggplot(nuc_norm_abundance %>% filter(is.finite(normalized_abundance)),
-                          aes(x = Nuclease, y = normalized_abundance, fill = reference_genome)) +
-  geom_jitter(aes(color = reference_genome),
-              position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.25),
-              size = 1.5, alpha = 0.7) +
-  geom_line(data = summary_norm_abundance %>% filter(mean_norm_abundance>0),
-            aes(x = Nuclease, y = mean_norm_abundance, group = reference_genome, color = reference_genome),
-            position = position_dodge(width = 0.25),
-            size = 0.75, alpha=0.5) +
-  geom_errorbar(data = summary_norm_abundance %>% filter(mean_norm_abundance>0),
-                inherit.aes=FALSE,
-                aes(x = Nuclease,
-                    ymin = mean_norm_abundance, 
-                    ymax = mean_norm_abundance,
-                    color=reference_genome),
-                position = position_dodge(width = 0.25), 
-                linewidth=1,
-                width=0.6) +
-  #facet_wrap(~reference_genome, ncol=1) +
+# Compute mean and standard error per virus and condition
+summary_nuc_norm_abundance <- nuc_norm_abundance %>%
+  group_by(reference_genome, Dose) %>%
+  summarise(
+    mean_log2FC = mean(log2FC, na.rm = TRUE),
+    se_log2FC = sd(log2FC, na.rm = TRUE) / sqrt(n()),
+    .groups = "drop"
+  ) %>%
+  mutate(Dose_log = ifelse(Dose == 0, 0.01, Dose))  # tiny value for log scale
+
+
+# Calculate the overall effect of nuclease for each virus
+nuc_VC_seq_spearman <- nuc_norm_abundance %>%
+  group_by(reference_genome) %>%
+  summarise(
+    rho = cor(log2FC, logDose, method = "spearman"),
+    p_value = cor.test(log2FC, logDose, method = "spearman")$p.value
+  ) %>%
+  mutate(star=case_when(p_value < 0.001 ~ "***",
+                        p_value < 0.01  ~ "**",
+                        p_value < 0.05  ~ "*",
+                        TRUE ~ "ns"))
+
+# Make log2 fold-change line plot
+nuc_VC_seq_plot <- ggplot(summary_nuc_norm_abundance, aes(x = Dose_log, y = mean_log2FC, color = reference_genome)) +
+  geom_line(aes(group = reference_genome), size = 1) + # lines connecting means
+  geom_point(size = 2) +  # mean points
+  geom_errorbar(aes(ymin = mean_log2FC - se_log2FC,
+                    ymax = mean_log2FC + se_log2FC),
+                width = 0.05, 
+                size = 0.5) + 
+  scale_x_log10(
+    limits=c(0.01, 20),
+    breaks=c(0.01, 0.1, 1, 10),
+    labels=c("untreated", "0.1X", "1X", "10X") # log10 scale for dose
+  ) +                                       
+  labs(x = "Nuclease treatment", y = "Log2 fold change compared to untreated") +
   scale_fill_paletteer_d("MoMAColors::Lupi") +
   scale_color_paletteer_d("MoMAColors::Lupi") +
-  scale_y_log10() +
-  labs(x = "Nuclease Treatment", y = "Relative Abundance normalized to no treatment (log10 scale)", 
-       title = "nuclease titration on mock community only \n(sequencing result)",
-       fill = "Reference Genome", color = "Reference Genome") + 
-  theme_minimal(base_family="Helvetica") +
-  theme(strip.text.x = element_text(size = 10),
-        strip.background = element_blank(),
-        axis.text.y = element_text(size=10))
-
-
+  theme_minimal()
 
 
 
 ############ qPCR plot
 # Get qpcr data
-setwd("/Users/jduan/bushman/virome_methods/paper/codes/codes for publication/")
-nuc_qpcr<-read_excel("250516_nuc_titration_qpcr.xlsx")
+setwd("/Users/jduan/bushman/virome_methods/VP protocol test/nuclease_titration")
+nuc_qpcr<-read_excel("250516_nuc_titration_qpcr_master.xlsx", sheet="for R analysis")
 nuc_qpcr$Nuclease <- factor(nuc_qpcr$Nuclease,
                             levels = c("none", "0.1X", "1X", "10X"))  # or your actual order
-nuc_qpcr_df <- nuc_qpcr %>% group_by(Group, Nuclease, Virus) %>% summarize(avg_VCN=mean(VCN)) # get the average across the qpcr values
-summary_nuc_qpcr <- nuc_qpcr_df %>% group_by(Nuclease, Virus) %>% summarize(avg_VCN_across_tri=mean(avg_VCN), sd_VCN=sd(avg_VCN)) #get the avg VCN across triplicates and the stdev
+nuc_qpcr_df <- nuc_qpcr %>% group_by(Group, Nuclease, Virus) %>% summarize(avg_VCN=mean(VCN)) %>% # get the average across the qpcr values
+  rename(reference_genome=Virus)
 
-# Calculate and plot for VCN normalized to the no treatment
-norm_nuc_qpcr <- summary_nuc_qpcr %>% filter(Nuclease=="none") %>% rename(no_nuclease_VCN=avg_VCN_across_tri) #get the no_nuclease avg VCN across triplicates
-norm_nuc_qpcr_df <- nuc_qpcr_df %>% left_join(norm_nuc_qpcr %>% ungroup() %>% select (Virus, no_nuclease_VCN),
-                                              by = c("Virus" = "Virus")) #join the table by matching the no_nuclease avg VCN for each nuclease group according to reference genome
-norm_nuc_qpcr_df <- norm_nuc_qpcr_df %>% mutate(normalized_VCN = avg_VCN/no_nuclease_VCN) #normalize the avg VCN of each nuclease treatment group by the no_treatment avg VCN value
-summary_norm_VCN <- norm_nuc_qpcr_df %>%
-  group_by(Nuclease, Virus) %>%
-  summarise(mean_norm_VCN = mean(normalized_VCN), .groups = "drop") # make a summary table of the mean of normalized VCN for triplicates
-summary_norm_VCN$percent_VCN <- paste0(round(summary_norm_VCN$mean_norm_VCN*100,1), "%") # convert to percentage
 
-# Make plot
-nuc_VC_qpcr_plot <- ggplot(norm_nuc_qpcr_df,
-                           aes(x = Nuclease, y = normalized_VCN, fill = Virus)) +
-  geom_jitter(aes(color = Virus),
-              position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.25),
-              size = 1.5, alpha = 0.7) +
-  geom_line(data = summary_norm_VCN,
-            aes(x = Nuclease, y = mean_norm_VCN, group = Virus, color = Virus),
-            position = position_dodge(width = 0.25),
-            size = 0.75, alpha=0.5) +
-  geom_errorbar(data = summary_norm_VCN %>% filter(mean_norm_VCN>0),
-                inherit.aes=FALSE,
-                aes(x = Nuclease, 
-                    ymin = mean_norm_VCN, 
-                    ymax = mean_norm_VCN,
-                    color=Virus),
-                position = position_dodge(width = 0.25), 
-                linewidth=1,
-                width=0.6) +
-  #facet_wrap(~Virus, ncol=1) +
-  scale_y_log10() +
+# Compute log2 fold change relative to untreated
+norm_nuc_qpcr <- nuc_qpcr_df %>% group_by(reference_genome) %>%
+  mutate(untreated_mean = mean(avg_VCN[Nuclease == "none"])) %>% #calculate the mean of untreated
+  ungroup() %>%
+  mutate(log2FC = log2((avg_VCN+1e-6)/untreated_mean)) #add pseudo value to avoid zero, and normalize other values to the mean of untreated
+
+# Encode nuclease numerically for regression
+norm_nuc_qpcr <- norm_nuc_qpcr %>%
+  mutate(Dose = case_when(
+    Nuclease == "none" ~ 0,
+    Nuclease == "0.1X"     ~ 0.1,
+    Nuclease == "1X"       ~ 1,
+    Nuclease == "10X"      ~ 10
+  ),
+  logDose = log10(Dose + 0.01))  # small offset to avoid log10(0)
+
+
+# Compute mean and standard error per virus and condition
+summary_norm_nuc_qpcr <- norm_nuc_qpcr %>%
+  group_by(reference_genome, Dose) %>%
+  summarise(
+    mean_log2FC = mean(log2FC, na.rm = TRUE),
+    se_log2FC = sd(log2FC, na.rm = TRUE) / sqrt(n()),
+    .groups = "drop"
+  ) %>%
+  mutate(Dose_log = ifelse(Dose == 0, 0.01, Dose))  # tiny value for log scale
+
+
+# Calculate the overall effect of nuclease for each virus
+nuc_VC_qpcr_spearman <- norm_nuc_qpcr %>%
+  group_by(reference_genome) %>%
+  summarise(
+    rho = cor(log2FC, logDose, method = "spearman"),
+    p_value = cor.test(log2FC, logDose, method = "spearman")$p.value
+  ) %>%
+  mutate(star=case_when(p_value < 0.001 ~ "***",
+                        p_value < 0.01  ~ "**",
+                        p_value < 0.05  ~ "*",
+                        TRUE ~ "ns"))
+
+# Make log2 fold-change line plot
+nuc_VC_qpcr_plot <- ggplot(summary_norm_nuc_qpcr, aes(x = Dose_log, y = mean_log2FC, color = reference_genome)) +
+  geom_line(aes(group = reference_genome), size = 1) +               
+  geom_point(size = 2) +                                  
+  geom_errorbar(aes(ymin = mean_log2FC - se_log2FC,
+                    ymax = mean_log2FC + se_log2FC),
+                width = 0.05,                             
+                size = 0.5) +                             
+  scale_x_log10(
+    limits=c(0.01, 20),
+    breaks=c(0.01, 0.1, 1, 10),
+    labels=c("untreated", "0.1X", "1X", "10X")
+  ) +     
+  labs(x = "Nuclease treatment", y = "Log2 fold change compared to untreated") +
   scale_fill_paletteer_d("MoMAColors::Lupi") +
   scale_color_paletteer_d("MoMAColors::Lupi") +
-  labs(x = "Nuclease Treatment", y = "Virus Copy Number normalized to no treatment (log10 scale)", 
-       title = "nuclease titration on mock community only \n(qPCR result)",
-       fill = "Virus", color = "Virus") + 
-  theme_minimal(base_family="Helvetica") +
-  theme(strip.text.x = element_text(size = 10),
-        strip.background = element_blank(),
-        axis.text.y = element_text(size=10))
-
-
+  theme_minimal()
 
 
 ############Sequencing plot (w/stool spike-in)
@@ -143,50 +171,91 @@ nuc_stool_ref_viral_abundance <- stool_ref_viral_abundance %>% filter(Experiment
 nuc_stool_ref_viral_abundance$Nuclease <- factor(nuc_stool_ref_viral_abundance$Nuclease,
                                                  levels = c("none", "0.1X", "1X", "10X"))  # or your actual order
 
-summary_nuc_stool_ref_viral_abundance <- nuc_stool_ref_viral_abundance %>%
-  filter(is.finite(ref_relative_abundance)) %>%
-  group_by(Nuclease, reference_genome) %>%
-  summarise(mean_abundance = mean(ref_relative_abundance), .groups = "drop")
+# Compute log2 fold change relative to untreated
+nuc_stool_norm_abundance <- nuc_stool_ref_viral_abundance %>% filter(Mock_Community=="mock community spiked-in") %>%
+  group_by(reference_genome) %>%
+  mutate(untreated_mean = mean(ref_relative_abundance[Nuclease == "none"])) %>% #calculate the mean of untreated
+  ungroup() %>%
+  mutate(log2FC = log2((ref_relative_abundance+1e-6)/untreated_mean)) #add pseudo value to avoid zero, and normalize other values to the mean of untreated
+
+# Encode nuclease numerically for regression
+nuc_stool_norm_abundance <- nuc_stool_norm_abundance %>%
+  mutate(Dose = case_when(
+    Nuclease == "none" ~ 0,
+    Nuclease == "0.1X"     ~ 0.1,
+    Nuclease == "1X"       ~ 1,
+    Nuclease == "10X"      ~ 10
+  ),
+  logDose = log10(Dose + 0.01))  # small offset to avoid log10(0)
+
+# Split between the last digit and the letter of Sample ID to create the sample and replicate columns
+nuc_stool_norm_abundance <- nuc_stool_norm_abundance %>% 
+  separate(Sample_ID, into=c("Sample", "replicate"), sep="(?<=\\d)(?=[A-Za-z])", remove=FALSE)
+nuc_stool_norm_abundance <- nuc_stool_norm_abundance %>% mutate(replicate=factor(replicate)) # make replicate a factor
+
+# Compute mean and standard error per virus and condition
+summary_nuc_stool_norm_abundance <- nuc_stool_norm_abundance %>%
+  group_by(reference_genome, Dose) %>%
+  summarise(
+    mean_log2FC = mean(log2FC, na.rm = TRUE),
+    se_log2FC = sd(log2FC, na.rm = TRUE) / sqrt(n()),
+    .groups = "drop"
+  ) %>%
+  mutate(Dose_log = ifelse(Dose == 0, 0.01, Dose))  # tiny value for log scale
 
 
-#### Calculate and plot for relative abundance normalized to the no treatment
-stool_norm_abundance <- summary_nuc_stool_ref_viral_abundance %>% filter(Nuclease=="none") %>% rename(no_nuclease_abundance=mean_abundance) #get the no_nuclease relative abundance
-nuc_stool_norm_abundance <- nuc_stool_ref_viral_abundance %>% left_join(stool_norm_abundance %>% ungroup() %>% select (reference_genome,no_nuclease_abundance),
-                                                                        by = c("reference_genome" = "reference_genome")) #join the table by matching the no_nuclease relative abundance value for each nuclease group according to reference genome
-nuc_stool_norm_abundance <- nuc_stool_norm_abundance %>% mutate(normalized_abundance = ref_relative_abundance/no_nuclease_abundance) #normalize the relative abundance of each nuclease treatment group by the no_treatment relative abundance value
+# Calculate the overall effect of nuclease for each virus
+nuc_VC_stool_seq_spearman <- nuc_stool_norm_abundance %>%
+  group_by(reference_genome) %>%
+  summarise(
+    rho = cor(log2FC, logDose, method = "spearman"),
+    p_value = cor.test(log2FC, logDose, method = "spearman")$p.value
+  ) %>%
+  mutate(star=case_when(p_value < 0.001 ~ "***",
+                        p_value < 0.01  ~ "**",
+                        p_value < 0.05  ~ "*",
+                        TRUE ~ "ns"))
 
-summary_stool_norm_abundance <- nuc_stool_norm_abundance %>%
-  filter(is.finite(ref_relative_abundance)) %>%
-  group_by(Nuclease, reference_genome) %>%
-  summarise(mean_norm_abundance = mean(normalized_abundance), .groups = "drop") #make a summary table of the mean of normalized abundance for triplicates
-
-nuc_VC_stool_seq_plot <- ggplot(nuc_stool_norm_abundance %>% filter(is.finite(normalized_abundance)),
-                                aes(x = Nuclease, y = normalized_abundance, fill = reference_genome)) +
-  geom_jitter(aes(color = reference_genome),
-              position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.25),
-              size = 1.5, alpha = 0.7) +
-  geom_line(data = summary_stool_norm_abundance %>% filter(mean_norm_abundance>0),
-            aes(x = Nuclease, y = mean_norm_abundance, group = reference_genome, color = reference_genome),
-            position = position_dodge(width = 0.25),
-            size = 0.75, alpha=0.5) +
-  geom_errorbar(data = summary_stool_norm_abundance %>% filter(mean_norm_abundance>0),
-                inherit.aes=FALSE,
-                aes(x = Nuclease, 
-                    ymin = mean_norm_abundance, 
-                    ymax = mean_norm_abundance,
-                    color=reference_genome),
-                position = position_dodge(width = 0.25), 
-                linewidth=1,
-                width=0.6) +
-  #facet_wrap(~reference_genome, ncol=1) +
-  scale_y_log10() +
+# Make log2 fold-change line plot
+nuc_VC_stool_seq_plot <- ggplot(summary_nuc_stool_norm_abundance, aes(x = Dose_log, y = mean_log2FC, color = reference_genome)) +
+  geom_line(aes(group = reference_genome), size = 1) +               
+  geom_point(size = 2) +                                  
+  geom_errorbar(aes(ymin = mean_log2FC - se_log2FC,
+                    ymax = mean_log2FC + se_log2FC),
+                width = 0.05,                             
+                size = 0.5) +                             
+  scale_x_log10(
+    limits=c(0.01, 20),
+    breaks=c(0.01, 0.1, 1, 10),
+    labels=c("untreated", "0.1X", "1X", "10X")
+  ) +                                       
+  labs(x = "Nuclease treatment", y = "Log2 fold change compared to untreated") +
   scale_fill_paletteer_d("MoMAColors::Lupi") +
   scale_color_paletteer_d("MoMAColors::Lupi") +
-  labs(x = "Nuclease Treatment", y = "Relative Abundance normalized to no treatment (log10 scale)", 
-       title = "nuclease titration on mock community spiked into stool \n(sequencing result)",
-       fill = "Reference Genome", color = "Reference Genome") + 
-  theme_minimal(base_family="Helvetica") +
-  theme(strip.text.x = element_text(size = 10),
-        strip.background = element_blank(),
-        axis.text.y = element_text(size=10))
+  theme_minimal()
+
+
+
+####################################### Save plots
+
+# get the plots that are going to be saved
+plots <- list(nuc_VC_seq_plot, nuc_VC_qpcr_plot, nuc_VC_stool_seq_plot)
+
+# specify the destination folder
+dest_folder <- "/Users/jduan/bushman/virome_methods/paper/manuscript/msystems_submission2/figure"
+if(!dir.exists(dest_folder)) dir.create(dest_folder, recursive = TRUE)
+
+# loop over plots and save as PDF
+for (i in seq_along(plots)) {
+  print(plots[[i]])   # ensures the plot is drawn
+  ggsave(
+    filename = file.path(dest_folder, paste0("Resub_Fig4_", i, ".pdf")),
+    plot = plots[[i]],
+    width = 6.5,
+    height = 7.5,
+    units = "in"
+    # no need to specify device; showtext handles font embedding
+  )
+}
+
 
